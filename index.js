9211f879f82715445eca1ef63e0d95306efcf74e
kj <kristoferjoseph@gmail.com>
2021-07-20 20:54:03 -0700

    Needs to export fragment.

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 let jsdom = require("jsdom")
-module.exports = function factory (input) {
+
+function factory (input) {
   return new jsdom.JSDOM(input)
 }
+
+factory.fragment = jsdom.JSDOM.fragment
+
+module.exports = factory
 // usage
 // let jsdom = require('@begin/jsdom')
 // let dom = jsdom('<p>omg hi</p>')
